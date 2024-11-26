6c7c003317c26f1028c7cb254e7ba6cfd3a362f8
Vinicius Nemetz Grabovski <vininemetz@gmail.com>
2024-11-26 16:52:31 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Reporistory for the Object Oriented Programming Subject's final project: a platform-game using C++, SFML graphics library and Object Oriented Programming.
---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Config.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    cout << "Working\n";
+    return 0;
+}
