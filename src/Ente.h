cb967ff69ff25266ce4879181049846ff7351406
Vinicius Nemetz Grabovski <vininemetz@gmail.com>
2024-11-27 16:26:03 -0300

    Declarando parte das classes base do diagrama de classes do Simão. Declaradas: Ente, Entidade, Gerenciador_Grafico, Jogo.

---
+++ src/Ente.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Jogo.h"
+#include "Gerenciador_Grafico.h"
+
+class Ente{
+protected:
+    int id;
+    static Gerenciador_Grafico* pGG;
+    //Figura* pFig;
+    //...
+
+public:
+    Ente(){}
+    ~Ente(){}
+    virtual void executar() = 0;
+    void desenhar();
+    //...
+};
