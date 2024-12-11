b83895c88e8d2f3e0ba7b2a1c0fbf62b9daa1f75
gab-borges <soliid002@gmail.com>
2024-12-11 01:07:22 -0300

    Adiciona Gerenciador de Input

---
+++ include/Gerenciador_Input.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "Jogador.h"
+#include <SFML/Window/Keyboard.hpp>
+#include <map>
+#include <functional>
+#include <SFML/Graphics.hpp>
+
+/* Gerenciadores - Namespace */
+namespace ger {
+class Gerenciador_Input {
+private:
+    std::map<sf::Keyboard::Key, std::function<void(bool)>> mapaTeclas;
+    pers::Jogador* jogador;
+
+    /* Singleton - Padrão de Projeto */
+    static Gerenciador_Input* instancia;
+
+private:
+    Gerenciador_Input();
+
+public:
+    ~Gerenciador_Input();
+
+    static Gerenciador_Input* getInstancia();
+
+    void setJogador(pers::Jogador* jog);
+    void incluir_tecla(sf::Keyboard::Key, std::function<void(bool)>);
+    
+    void teclaApertada(sf::Keyboard::Key tecla);
+    void teclaSoltada(sf::Keyboard::Key tecla);
+};
+}
