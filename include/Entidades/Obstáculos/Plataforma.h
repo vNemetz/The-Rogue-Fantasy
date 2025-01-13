dc4488b99f14f418797eea35dc2efc62091f9af5
gab-borges <soliid002@gmail.com>
2025-01-13 12:22:05 -0300

    Cria o diretório Obstáculos

---
+++ include/Entidades/Obstáculos/Plataforma.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Entidades/Personagens/Personagem.h"
+#include "Entidades/Obstáculos/Obstaculo.h"
+
+namespace ent {
+namespace obs {
+
+class Plataforma : public Obstaculo {
+private:
+    int altura;
+
+public:
+    Plataforma();
+    Plataforma(sf::Vector2f pos, sf::Vector2f tam, bool dan, int alt);
+    ~Plataforma();
+
+    void executar();
+    void emColisao(Entidade* pE, sf::Vector2f ds);
+    void emColisaoPersonagem(pers::Personagem* pPers, sf::Vector2f ds);
+};
+
+}
+}
---
+++ src/Entidades/Obstáculos/Obstaculo.cpp
@@ -0,0 +1,21 @@
+#include "Entidades/Obstáculos/Obstaculo.h"
+
+namespace ent {
+namespace obs {
+
+Obstaculo::Obstaculo()
+    : Obstaculo(sf::Vector2f(0.f, 0.f), sf::Vector2f(0.f, 0.f), false, vazio)
+{
+}
+
+Obstaculo::Obstaculo(sf::Vector2f pos, sf::Vector2f tam, bool dan, ID id) 
+    : Entidade(pos, tam, id)
+    , danoso(dan)
+{
+}
+
+Obstaculo::~Obstaculo() {
+}
+
+}
+}
